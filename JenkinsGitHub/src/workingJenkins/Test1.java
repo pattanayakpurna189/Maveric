e92f1d0043e9dd6bc016855b65a71e58326b69a4
Purna Pattanayak <pattanayak.purna@outlook.com>
2018-03-27 18:35:08 +0530

    Test2

--- JenkinsGitHub/src/workingJenkins/Test1.java
+++ JenkinsGitHub/src/workingJenkins/Test1.java
@@ -6,10 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test1 {
 	public static void main(String[] args)
 	{
-		WebDriver driver = new ChromeDriver();
+		/*WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.co.in");
 		driver.manage().window().maximize();
-		
+		*/
+		System.out.println("Tested Successfully");
 		
 	}
 
---
+++ JenkinsGitHub/src/workingJenkins/Test2.java
@@ -0,0 +1,9 @@
+package workingJenkins;
+
+public class Test2 {
+	public static void main(String[] args)
+	{
+			System.out.println("Tested Successfully");
+		
+	}
+}
